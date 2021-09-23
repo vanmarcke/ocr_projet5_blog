2fd06ba5592a3cc4fd1127dc9af65f01dbdae7de
Frédéric Vanmarcke <fvanmarcke72@gmail.com>
2021-09-23 10:43:42 +0200

    Movable scripts put in the head

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
     <!-- Head site ***************************************** -->
     <title>Frédéric Vanmarcke | Portfolio - Développeur & Intégrateur Web</title>
 
-    <link rel="manifest" href="public/manifest.json" />
-
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="application-name" content="VMKDEV">
@@ -33,6 +31,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="manifest" href="public/manifest.json" />
+
+    <!-- CSS Transition and Transformation script  -->
+    <script src="https://unpkg.com/scrollreveal"></script>
 
     <!-- Global site tag (gtag.js) - Function for Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-B8PVFKPPGT"></script>
@@ -40,9 +42,19 @@
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'G-B8PVFKPPGT');
     </script>
+
+    <!-- reCAPTCHA v3 - Load the JavaScript API with my sitekey. -->
+    <script src="https://www.google.com/recaptcha/api.js?render=6LeytAEbAAAAAJbWsEdMqp7_npryrc-itzfYYxS9"></script>
+    <!-- Call grecaptcha.execute on each send action of the contact form.  -->
+    <script>
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LeytAEbAAAAAJbWsEdMqp7_npryrc-itzfYYxS9', {action: 'submit'}).then(function(token) {
+            document.getElementById('recaptchaResponse').value = token
+        });
+    });
+    </script>  
 
 </head>
 
@@ -247,19 +259,6 @@
 
     <!-- Scripts ***************************************** -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-    <script src="https://www.google.com/recaptcha/api.js?render=6LeytAEbAAAAAJbWsEdMqp7_npryrc-itzfYYxS9"></script>
-
-    <script>
-    grecaptcha.ready(function() {
-        grecaptcha.execute('6LeytAEbAAAAAJbWsEdMqp7_npryrc-itzfYYxS9', {action: 'submit'}).then(function(token) {
-            document.getElementById('recaptchaResponse').value = token
-        });
-    });
-    </script>  
-
-    <script src="https://unpkg.com/scrollreveal"></script>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="public/js/app.js" async defer></script>
