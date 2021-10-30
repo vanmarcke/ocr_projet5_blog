ea0804e6f38787c7e4f2324a85e46833a6868ff2
Frédéric Vanmarcke <fvanmarcke72@gmail.com>
2021-10-30 18:32:05 +0200

    Corrections according to github comments

--- src/controller/Router.php
+++ src/controller/Router.php
@@ -40,91 +40,111 @@ class Router
 		if (isset($_GET['url'])) {
 			$url = explode('/', strtolower($_GET['url']));
 		}
-		/*Accueil*/
-		if ($url == '' || $url[0] == 'accueil') {
-			$homepageController = new HomepageController();
-			$homepageController->index($userModel);
 
-			/*Les Articles*/
-		} elseif (preg_match('#^articles?-page([0-9]+)$#', $url[0], $params)) {
-			$currentPage = intval($params[1]);
-			$postController = new FrontPostController();
-			$postController->displayPosts($postModel, $currentPage);
-			$url = '';
-
-			/*Un article-id with this comments*/
-		} elseif (preg_match('#^articles?-([0-9]+)-page([0-9]+)$#', $url[0], $params)) {
-			$idPost = $params[1];
-			$currentPage = intval($params[2]);
-			$postController = new FrontPostController();
-			$postController->displayPost($postModel, $commentModel, $idPost, $currentPage);
-
-			/*ajouter-un-commentaire-idPost*/
-		} elseif (preg_match('#^ajouter-un-commentaire-([0-9]+)$#', $url[0], $params)) {
-			$idPost = $params[1];
-			$commentController = new CommentController();
-			$commentController->insertComment($postModel, $commentModel, $idPost);
-
-			/*ajouter-un-article*/
-		} elseif ($url[0] == 'ajouter-un-article') {
-			$postController = new BackPostController();
-			$postController->addPost($postModel);
-
-			/*supprimer-un-article*/
-		} elseif (preg_match('#^supprimer-article-([0-9]+)$#', $url[0], $params)) {
-			$idPost = $params[1];
-			$postController = new BackPostController();
-			$postController->deletePost($idPost, $postModel);
-
-			/*modifier-article-id*/
-		} elseif (preg_match('#^modifier-article-([0-9]+)$#', $url[0], $params)) {
-			$idPost = $params[1];
-			$postController = new BackPostController();
-			$postController->editPost($idPost, $postModel);
-
-			/*connexion*/
-		} elseif ($url[0] == 'connexion') {
-			$userController = new UserController();
-			$userController->connexion($userModel);
-
-			/*inscription*/
-		} elseif ($url[0] == 'inscription') {
-			$userController = new UserController();
-			$userController->register($userModel);
-
-			/*deconnexion*/
-		} elseif ($url[0] == 'deconnexion') {
-			$userController = new UserController();
-			$userController->disconnect();
-
-			/*administratrion*/
-		} elseif ($url[0] == 'administration') {
-			$adminController = new AdminController();
-			$adminController->displayAllElements($userModel, $postModel, $commentModel);
-
-			/*admin-posts*/
-		} elseif ($url[0] == 'admin-waiting-posts') {
-			$adminController = new AdminController();
-			$adminController->displayWaitingPosts($userModel, $postModel, $commentModel);
-
-			/*admin-users*/
-		} elseif ($url[0] == 'admin-pending-users') {
-			$adminController = new AdminController();
-			$adminController->displayPendingUsers($userModel, $postModel, $commentModel);
-
-			/*admin-waiting-comments-*/
-		} elseif ($url[0] == 'admin-waiting-comments') {
-			$adminController = new AdminController();
-			$adminController->displayInvalidComments($userModel, $postModel, $commentModel);
-
-			/*admin-refused-comments-*/
-		} elseif ($url[0] == 'admin-refused-comments') {
-			$adminController = new AdminController();
-			$adminController->displayRefusedComments($userModel, $postModel, $commentModel);
-		} else {
-			$_SESSION['error'] = 'Erreur 404 - Page non trouvé';
-			$homepageController = new HomepageController();
-			$homepageController->index($userModel);
+		switch (true) {
+				/*Accueil*/
+			case ($url == '' || $url[0] == 'accueil'): {
+					$homepageController = new HomepageController();
+					$homepageController->index($userModel);
+					break;
+				}
+				/*Les Articles*/
+			case (preg_match('#^articles?-page([0-9]+)$#', $url[0], $params)): {
+					$currentPage = intval($params[1]);
+					$postController = new FrontPostController();
+					$postController->displayPosts($postModel, $currentPage);
+					$url = '';
+					break;
+				}
+				/*Un article-id with this comments*/
+			case (preg_match('#^articles?-([0-9]+)-page([0-9]+)$#', $url[0], $params)): {
+					$idPost = $params[1];
+					$currentPage = intval($params[2]);
+					$postController = new FrontPostController();
+					$postController->displayPost($postModel, $commentModel, $idPost, $currentPage);
+					break;
+				}
+				/*ajouter-un-commentaire-idPost*/
+			case (preg_match('#^ajouter-un-commentaire-([0-9]+)$#', $url[0], $params)): {
+					$idPost = $params[1];
+					$commentController = new CommentController();
+					$commentController->insertComment($postModel, $commentModel, $idPost);
+					break;
+				}
+				/*ajouter-un-article*/
+			case ($url[0] == 'ajouter-un-article'): {
+					$postController = new BackPostController();
+					$postController->addPost($postModel);
+					break;
+				}
+				/*supprimer-un-article*/
+			case (preg_match('#^supprimer-article-([0-9]+)$#', $url[0], $params)): {
+					$idPost = $params[1];
+					$postController = new BackPostController();
+					$postController->deletePost($idPost, $postModel);
+					break;
+				}
+				/*modifier-article-id*/
+			case (preg_match('#^modifier-article-([0-9]+)$#', $url[0], $params)): {
+					$idPost = $params[1];
+					$postController = new BackPostController();
+					$postController->editPost($idPost, $postModel);
+					break;
+				}
+				/*connexion*/
+			case ($url[0] == 'connexion'): {
+					$userController = new UserController();
+					$userController->connexion($userModel);
+					break;
+				}
+				/*inscription*/
+			case ($url[0] == 'inscription'): {
+					$userController = new UserController();
+					$userController->register($userModel);
+					break;
+				}
+				/*deconnexion*/
+			case ($url[0] == 'deconnexion'): {
+					$userController = new UserController();
+					$userController->disconnect();
+					break;
+				}
+				/*administratrion*/
+			case ($url[0] == 'administration'): {
+					$adminController = new AdminController();
+					$adminController->displayAllElements($userModel, $postModel, $commentModel);
+					break;
+				}
+				/*admin-posts*/
+			case ($url[0] == 'admin-waiting-posts'): {
+					$adminController = new AdminController();
+					$adminController->displayWaitingPosts($userModel, $postModel, $commentModel);
+					break;
+				}
+				/*admin-users*/
+			case ($url[0] == 'admin-pending-users'): {
+					$adminController = new AdminController();
+					$adminController->displayPendingUsers($userModel, $postModel, $commentModel);
+					break;
+				}
+				/*admin-waiting-comments-*/
+			case ($url[0] == 'admin-waiting-comments'): {
+					$adminController = new AdminController();
+					$adminController->displayInvalidComments($userModel, $postModel, $commentModel);
+					break;
+				}
+				/*admin-refused-comments-*/
+			case ($url[0] == 'admin-refused-comments'): {
+					$adminController = new AdminController();
+					$adminController->displayRefusedComments($userModel, $postModel, $commentModel);
+					break;
+				}
+			default; {
+					$_SESSION['error'] = 'Erreur 404 - Page non trouvé';
+					$homepageController = new HomepageController();
+					$homepageController->index($userModel);
+					break;
+				}
 		}
 	}
 
